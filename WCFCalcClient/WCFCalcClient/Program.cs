fc81a6641042ca9f38b9aca21999cbaabaa21250
Matias <Matias@DESKTOP-TA0TGEP>
2017-09-13 11:14:23 +0200

    Service calculator

---
+++ WCFCalcClient/WCFCalcClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFCalcClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new CalcService.CalcServiceClient())
+            {
+                double test = client.Minus(10, 9);
+
+                Console.WriteLine(test);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WCFServiceCalculator/WCFServiceCalculator/CalcService.svc.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFServiceCalculator
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CalcService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select CalcService.svc or CalcService.svc.cs at the Solution Explorer and start debugging.
+    public class CalcService : ICalcService
+    {
+        public double Divider(double a, double b)
+        {
+            return a/b;
+        }
+
+        public double Gange(double a, double b)
+        {
+            return a * b;
+
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public double Minus(double a, double b)
+        {
+            return a - b;
+
+        }
+
+        public double plus(double a, double b)
+        {
+            return a + b;
+
+        }
+    }
+}
