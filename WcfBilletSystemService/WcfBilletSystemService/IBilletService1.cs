bcf9ff5037e9740e546bf63a9bdda87f33aa81e1
Matias <Matias@DESKTOP-TA0TGEP>
2017-09-20 07:21:05 +0200

    test

--- WcfBilletSystemService/WcfBilletSystemService/IBilletService1.cs
+++ WcfBilletSystemService/WcfBilletSystemService/IBilletService1.cs
@@ -8,7 +8,7 @@ using System.Text;
 
 namespace WcfBilletSystemService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IBilletService1" in both code and config file together.
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IBilletService1" in both code and config file together..
     [ServiceContract]
     public interface IBilletService1
     {
