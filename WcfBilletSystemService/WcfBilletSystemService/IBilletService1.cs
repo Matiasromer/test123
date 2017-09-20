5c59a75e7e32d47ed97b932dba577d63a69b5973
Matias <Matias@DESKTOP-TA0TGEP>
2017-09-20 07:15:55 +0200

    lavet forskellige metoder til mc og bil samt mc øresund og bil øresund

---
+++ WcfBilletSystemService/WcfBilletSystemService/IBilletService1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfBilletSystemService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IBilletService1" in both code and config file together.
+    [ServiceContract]
+    public interface IBilletService1
+    {
+
+        
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+        [OperationContract]
+        int BilPris();
+
+        [OperationContract]
+        int MCPris();
+
+        [OperationContract]
+        int BilMedRabat();
+
+        [OperationContract]
+        int MCØresund();
+
+        [OperationContract]
+        int BilØresund();
+
+        [OperationContract]
+        int BilØresundRabat();
+
+        [OperationContract]
+        int MCØresundRabat();
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ WcfBilletSystemService/WcfBilletSystemService/Service1.svc.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using BilletLib;
+
+namespace WcfBilletSystemService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "BilletService1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select BilletService1.svc or BilletService1.svc.cs at the Solution Explorer and start debugging.
+    public class BilletService1 : IBilletService1
+    {
+        public int BilMedRabat()
+        {
+            var bilRabat = new Bil();
+            bilRabat.BroBizz = true;
+            return bilRabat.Pris();
+        }
+
+        public int BilPris()
+        {
+            var bil = new Bil();
+            return bil.Pris();
+        }
+
+        public int BilØresund()
+        {
+            var biløresund = new BilØresund();
+            return biløresund.Pris();
+        }
+
+        public int BilØresundRabat()
+        {
+            var biløresundrabat = new BilØresund();
+            biløresundrabat.BroBizz = true;
+            return biløresundrabat.Pris();
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        
+
+        public int MCPris()
+        {
+            var mc = new MC();
+            return mc.Pris();
+        }
+
+        public int MCØresund()
+        {
+            var mcøresund = new MCØresund();
+            return mcøresund.Pris();
+        }
+
+        public int MCØresundRabat()
+        {
+            var mcøresundrabat = new MCØresund();
+            mcøresundrabat.BroBizz = true;
+            return mcøresundrabat.Pris();
+        }
+    }
+}
